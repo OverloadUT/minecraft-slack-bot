301c3a949abecbdfeb1a54f79fb922b6e23c7a06
Greg Laabs <OverloadUT@gmail.com>
2015-03-11 17:10:15 -0700

    Add support for chatNames as JSON

--- config-heroku.js
+++ config-heroku.js
@@ -16,4 +16,6 @@ config.incomingChatServerPort = process.env.PORT
 config.incomingChatToken = process.env.CHAT_SERVER_TOKEN
 config.incomingChatPrivacy = process.env.CHAT_PRIVACY
 
+config.chatNames = JSON.parse(process.env.CHAT_NAMES)
+
 module.exports = config;
